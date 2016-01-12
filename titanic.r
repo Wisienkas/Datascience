760fa956eab6e01ab24eb88eb32423384b90420b
Nikolaj Reibke <yturi@DESKTOP-L10F5SS.(none)>
2016-01-13 00:00:39 +0100

    Added Eclat And Apiori

---
+++ titanic.r
@@ -0,0 +1,17 @@
+install.packages("arules")
+library("arules")
+load("./titanic.raw.rdata")
+
+rules_apriori <- apriori(titanic.raw, 
+                 parameter = list(supp = 0.01, conf = 0.80, minlen = 2, 
+                                  target = "closed frequent itemsets"))
+
+rules_apriori.sorted <- sort(rules_apriori, by="support")
+inspect(rules_apriori.sorted[1:10])
+
+rules_eclat <- eclat(titanic.raw, 
+      parameter = list(supp = 0.01, minlen = 2, 
+                       target = "closed frequent itemsets"))
+
+rules_eclat.sorted <- sort(rules_eclat, by="support")
+inspect(rules_eclat.sorted[1:10])
